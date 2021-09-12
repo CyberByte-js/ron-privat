2be0ebaa2e669603ba417dd3aa441053fb12071e
CyberByte-js <71409005+CyberByte-js@users.noreply.github.com>
2021-09-12 17:36:43 -0300

    trojan dialogue

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -388,13 +388,13 @@ class PlayState extends MusicBeatState
 			case 'trojan-virus':
 				{					
 					dialogue = [
-						":dad: I will commit genocide on the entire human race if you dont give up rn",
-						":dad: Like deadass Ill kill all of " + user + " s family",
-						":bf: who is that",
-						":dad: what",
-						":bf: i do not know that person",
-						":dad: mmhwhatever just know that im going to kill you",
-						":bf: guh??",
+						":dad:I will commit genocide on the entire human race if you dont give up rn",
+						":dad:Like deadass Ill kill all of " + user + "s family",
+						":bf:who is that",
+						":dad:what",
+						":bf:i do not know that person",
+						":dad:mmhwhatever just know that im going to kill you",
+						":bf:guh??",
 						];
 				}
 		}
