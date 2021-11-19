abbcad22c11c033b0ea20caaf7781f50f751085b
Busybody64 <92069026+Busybody64@users.noreply.github.com>
2021-11-18 18:08:18 -0600

    Update HealthIcon.hx
    susron

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -57,7 +57,8 @@ class HealthIcon extends FlxSprite
 		animation.add('factorytankman-2', [42, 43], 0, false, isPlayer);
 		animation.add('hellron-2', [38, 39], 0, false, isPlayer);
 		animation.add('oldhellron', [44, 45], 0, false, isPlayer);
-		animation.add('gf-in', [46], 0, false, isPlayer);
+		animation.add('susron', [46, 47], 0, false, isPlayer);
+		animation.add('gf-in', [48], 0, false, isPlayer);
 		animation.play(char);
 
 		switch(char)
